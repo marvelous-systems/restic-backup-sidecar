2e9e62ee4d5f16061c3baa071939d05e9c4f3a39
Noah Hummel <n.p.hummel@gmail.com>
2019-01-29 16:13:54 +0100

    [FEATURE] check if repo exists and initialize if missing

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Sidecar container which runs restic using sftp backend configured by environment using cron.
+Sidecar container for backing up volumes using resting over sftp.
+
+## Build
+
+```bash
+docker build -t restic-sidecar .
+```
+
+## Run
+
+```bash
+docker run -v /path/to/id_rsa:/restic/key --env-file ./environment.env restic-sidecar
+```
+
+## Notes
+
+When running in production, ssh key and `REPO_PASSWORD` should be provided by secrets, otherwise they might leak.
--- scripts/prepare-repo.sh
+++ scripts/prepare-repo.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
-echo "Preparing repository..."
+echo "Checking repository..."
+
 ./restic \
 	-r "sftp::${SFTP_PATH}" \
-	-o sftp.command="ssh ${SFTP_USER}@${SFTP_HOST} -p ${SFTP_PORT} -i /restic/key -s sftp" \
-	init
+	-o sftp.command="ssh ${SFTP_USER}@${SFTP_HOST} -p ${SFTP_PORT} -o StrictHostKeyChecking=no -i /restic/key -s sftp" \
+	snapshots > /dev/null
+
+ret=$?
+if [[ $ret != 0 ]]
+then
+	# something went wrong, repo possibly doesn't exist
+	echo "Creating repository..."
+	./restic \
+		-r "sftp::${SFTP_PATH}" \
+		-o sftp.command="ssh ${SFTP_USER}@${SFTP_HOST} -p ${SFTP_PORT} -o StrictHostKeyChecking=no -i /restic/key -s sftp" \
+		init
+fi
+
+rc=$?
+if [[ $rc != 0 ]]
+then 
+	echo "Can't recover, aborting."
+	exit 10
+fi
