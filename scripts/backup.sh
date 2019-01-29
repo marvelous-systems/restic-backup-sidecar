264a24ac57b95fdb2372fe43fb01a2eb5cfc74d2
Noah Hummel <n.p.hummel@gmail.com>
2019-01-29 17:51:14 +0100

    [FEATURE] backup given paths

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 echo "Starting backup..."
+for path in $(echo $BACKUP_PATHS | sed "s/,/ /g")
+do
+	./restic \
+		-r "sftp::${SFTP_PATH}" \
+		-o sftp.command="ssh ${SFTP_USER}@${SFTP_HOST} -p ${SFTP_PORT} -o StrictHostKeyChecking=no -i /restic/key -s sftp" \
+		--verbose \
+		backup "$path"
+done
